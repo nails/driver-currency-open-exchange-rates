00a89739c49c427b5c256fbfb2cf28d88f2ce8fa
Pablo de la Peña <p@shedcollective.org>
2020-08-24 15:55:17 +0100

    Adds support for querying the OER API

--- src/OpenExchangeRates.php
+++ src/OpenExchangeRates.php
@@ -3,6 +3,10 @@
 namespace Nails\Currency\Driver;
 
 use Nails\Common\Driver\Base;
+use Nails\Common\Factory\HttpRequest;
+use Nails\Common\Service\HttpCodes;
+use Nails\Currency\Exception\ExchangeException\DriverApiException;
+use Nails\Currency\Exception\ExchangeException\DriverNotConfiguredException;
 use Nails\Currency\Interfaces\Driver;
 use Nails\Currency\Resource\Currency;
 use Nails\Factory;
@@ -11,6 +15,7 @@ class OpenExchangeRates extends Base implements Driver
 {
     /**
      * The base url of the Open Exchange Rates service
+     *
      * @var string
      */
     const BASE_URL = 'https://openexchangerates.org/api';
@@ -19,6 +24,7 @@ class OpenExchangeRates extends Base implements Driver
 
     /**
      * The App ID to use.
+     *
      * @var string
      */
     protected $sAppId;
@@ -35,6 +41,47 @@ class OpenExchangeRates extends Base implements Driver
      */
     public function getRate(Currency $oFrom, Currency $oTo): float
     {
-        return 0.22;
+        if (empty($this->sAppId)) {
+            throw new DriverNotConfiguredException(
+                'The OpenExchangeRate App ID has not been configured'
+            );
+        }
+
+        //  @todo (Pablo 24/08/2020) - Cache the request so subsequent queries for the same base are faster
+
+        /** @var HttpRequest\Get $oGet */
+        $oGet      = Factory::factory('HttpRequestGet');
+        $oResponse = $oGet
+            ->baseUri(static::BASE_URL)
+            ->path('latest.json')
+            ->query([
+                'app_id' => $this->sAppId,
+                'base'   => $oFrom->code,
+            ])
+            ->execute();
+
+        if ($oResponse->getStatusCode() !== HttpCodes::STATUS_OK) {
+            throw new DriverApiException(
+                sprintf(
+                    'Received a non-200 response from OER API: %s %s',
+                    $oResponse->getStatusCode(),
+                    $oResponse->getReasonPhrase()
+                )
+            );
+        }
+
+        $oBody = $oResponse->getBody();
+
+        if (!property_exists($oBody->rates, $oTo->code)) {
+            throw new DriverApiException(
+                sprintf(
+                    'Currency "%s" was not found in returned exchange rates for currency %s',
+                    $oTo->code,
+                    $oFrom->code
+                )
+            );
+        }
+
+        return (float) $oBody->rates->{$oTo->code};
     }
 }
